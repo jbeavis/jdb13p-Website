a437f36fd7301abb298e69de192a75b2aed3e710
James Beavis <beavisj1@cardiff.ac.uk>
2024-02-19 21:45:06 +0000

    Turned all old logs into txt

--- log.php
+++ log.php
@@ -33,15 +33,6 @@
                         echo "</p><hr>";
                     }
                 ?>
-                <h3>23/01/24</h3>
-                <p>I've added some photos to the gallery. I also took some photos out of the gallery. Which will be added in at a later date!! <br> I'm learning a lot on my work placement. Which is great, but it also makes me feel like this website could use a MAJOR refactor.</p>
-                <hr>
-                <h3>10/09/23</h3>
-                <p>It's been a while! First of all... I'm still not happy with how this site looks lol. It's a little bit ugly. I also want to make an entrance page, but not right now bc that'd mean changing all links that reference the current index page. <br>I've officially moved to the town where I'm going to do my work placement. Idk how I feel about it just yet. </p>
-                <hr>
-                <h3>22/06/23</h3>
-                <p>I've redesigned the site! I didn't like the side bar, or the way I was using Tumblr to host my photos, (or the way I wrote everything in lowercase...) so I made this instead. It looks pretty similar, I used the same colours and fonts, but it was made mostly from scratch. I've been very bored since uni ended and I went home to my family haha... I'll see if I can find a way to host my old logs here somewhere.</p>
-                <hr>
             </div>
         </div>
     </body>
